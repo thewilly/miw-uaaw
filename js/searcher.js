de8b6a8d0cf6be6e607c50529ca2863a205fac23
Willy <10364506+thewilly@users.noreply.github.com>
2020-12-14 12:15:53 +0100

    Add files via upload

---
+++ js/searcher.js
@@ -0,0 +1,63 @@
+window.onload = function(){
+    let searchBtn = document.getElementById('searchBtn');
+    document.getElementById("searchResults").style.display = "none";
+    searchBtn.addEventListener("click",function(){
+        search();
+    })
+}
+
+async function search(){
+    document.getElementById("searchResults").style.display = "block";
+    
+    $('#searchResults').empty();
+    let criteria = document.getElementById("searchedText").value.toLowerCase();
+      
+    let results = 0;
+ 
+    $.get( "/index.html", function( data ) {
+       let content = data;
+       let result = content.toLowerCase().search(criteria)
+        if(result!=-1){
+            $('#searchResults').append($("<p><a href='/index.html'>Inicio</a></p>"))
+            results++;
+        }
+    }); 
+
+    $.get( "/aficiones.html", function( data ) {
+        let content = data;
+        let result = content.toLowerCase().search(criteria)
+        if(result!=-1){
+            $('#searchResults').append($("<p><a href='/aficiones.html'>Aficiones</a></p>"))
+            results++;
+        }
+      });
+
+    $.get( "/publicaciones.html", function( data ) {
+        let content = data;
+       let result = content.toLowerCase().search(criteria)
+       if(result!=-1){
+            $('#searchResults').append($("<p><a href='/publicaciones.html'>Publicaciones</a></p>"))
+            results++;
+       }
+      });
+
+    await $.get( "/proyectos.html", function( data ) {
+        let content = data;
+        let result = content.toLowerCase().search(criteria)
+        if(result!=-1){
+            $('#searchResults').append($("<p><a href='/proyectos.html'>Proyectos</a></p>"))
+            results++;
+        }
+      });
+
+
+  
+    if(results<=0){
+        $('#searchResults').append("<div><h2>Lo siento, se han encontrado </h2><p>("+results+") resultados para \""+criteria+"\"</p></div>")
+        console.log(results);
+    }else{
+        $('#searchResults').prepend("<div><h2>Se han encontrado </h2><p>("+results+") resultados para \""+criteria+"\"</p></div>")
+        console.log(results);
+        
+    }
+}
